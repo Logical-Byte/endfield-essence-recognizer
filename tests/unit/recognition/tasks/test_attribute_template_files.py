d050cc67b907f80970de96f89232f106229d1bf8
MisakaVan <2102315149@qq.com>
2026-02-04 03:34:02 +0800

    ci: add CI option to pytest and skip tests requiring JSON files

--- tests/unit/recognition/tasks/test_attribute_template_files.py
+++ tests/unit/recognition/tasks/test_attribute_template_files.py
@@ -13,6 +13,10 @@ def get_attribute_templates():
 @pytest.mark.parametrize(
     "template_descriptor", get_attribute_templates(), ids=lambda d: d.label
 )
+@pytest.mark.skip_in_ci(
+    reason="CI environment does not include json files."
+    " Currently the build_attribute_profile function depends on json files."
+)
 def test_attribute_template_file_exists(template_descriptor):
     """
     Test that each attribute template file exists in the repository.
