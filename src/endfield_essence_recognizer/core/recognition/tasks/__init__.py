270405ad363e82385072917fce8559c8e138ccef
MisakaVan <2102315149@qq.com>
2026-02-03 23:46:19 +0800

    refactor: rename status.py to locked_deprecated_status.py

--- src/endfield_essence_recognizer/core/recognition/tasks/__init__.py
+++ src/endfield_essence_recognizer/core/recognition/tasks/__init__.py
@@ -1,4 +1,4 @@
 from .attribute import build_attribute_profile
-from .status import StatusLabel, build_status_profile
+from .locked_deprecated_status import StatusLabel, build_status_profile
 
 __all__ = ["build_attribute_profile", "StatusLabel", "build_status_profile"]
